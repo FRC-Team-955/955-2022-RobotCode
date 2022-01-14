3956ae7b1b793cbf5a60234910e40df15bdba674
LukeWilliams <47074748+LukeWilliams271@users.noreply.github.com>
2022-01-13 19:40:12 -0800

    Fixed spelling on drivebase

---
+++ src/main/cpp/drivebase.cpp
@@ -0,0 +1,10 @@
+#include "drivebase.h"
+
+void DriveBase::Drive(){
+	
+    bool isQuickTurn = joystick.GetRawButton(0);
+	m_rightFollowMotor.Follow(m_rightLeadMotor);
+    m_leftFollowMotor.Follow(m_leftLeadMotor);
+    m_robotDrive.CurvatureDrive(joystick.GetRawAxis(1), joystick.GetRawAxis(4), isQuickTurn);
+    //m_pidController.SetReference(rotations, rev::ControlType::kPosition);
+}
