d42ed3c6a88f67078f3b40607c6fa77d0b5ff4a4
LukeWilliams <47074748+LukeWilliams271@users.noreply.github.com>
2022-01-20 20:03:23 -0800

    Added vertical align

--- src/main/cpp/xy_align.cpp
+++ src/main/cpp/xy_align.cpp
@@ -9,14 +9,20 @@ using namespace frc;
 void XYalign::test()
 {
   float Kp = -0.1f;
+  float KpDistance = -0.1f;
+
   float min_command = 0.05f;
 
   auto table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
   float tx = table->GetNumber("tx", 0.0);
+  float ty = table->GetNumber("ty", 0.0);
+
 
   if (joystick.GetRawButton(9))
   {
     float heading_error = -tx;
+    float distance_error = -ty;
+
     float steering_adjust = 0.0f;
     /*if (tx > 1.0)
     {
@@ -26,6 +32,7 @@ void XYalign::test()
     {
       steering_adjust = Kp*heading_error + min_command;
     }*/
+    float distance_adjust = KpDistance * distance_error; //Set the variable in shooter code
     steering_adjust = Kp*heading_error;
 
     drivebase->DriveTank(steering_adjust, -steering_adjust);
