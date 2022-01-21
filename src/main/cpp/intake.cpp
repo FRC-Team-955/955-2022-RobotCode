26a7edbfaf37c9f8d07286d79b21a2b9263845f2
MSdS1 <97410996+MSdS1@users.noreply.github.com>
2022-01-20 20:40:02 -0800

    Added a timer, not sure if it works yet

--- src/main/cpp/intake.cpp
+++ src/main/cpp/intake.cpp
@@ -1,5 +1,34 @@
 #include "intake.h"
+#include "units/time.h"
 using namespace frc;
 
+void Intake::intake_run(){
+if(joy1.GetRawButton(1) == true){
+    sol1.Set(1);
+    sol2.Set(1);
+}
+else if(joy1.GetRawButton(2) == true){
+    sol1.Set(0);
+    sol2.Set(0);
+}
+if(joy1.GetRawButton(3) == true){
+    t1.Set(ControlMode::PercentOutput, .5);
+}
+if(t1.GetOutputCurrent() > 10){
+    t1.Set(ControlMode::PercentOutput, -.5);
+}
+else if(joy1.GetRawButton(3) == true){
+    t1.Set(ControlMode::PercentOutput, 0);
+}
+
+if(joy1.GetRawButton(4) == true){
+    t2.Set(ControlMode::PercentOutput, .1);
+    time1.Start();
+    if(time1.HasElapsed(units::second_t (2)) == true){
+        t2.Set(ControlMode::PercentOutput, 0);
+    }
+}
+    time1.Reset();
+}
 
 
--- src/main/include/intake.h
+++ src/main/include/intake.h
@@ -3,12 +3,22 @@
 #include <frc/Solenoid.h>
 #include <frc/Joystick.h>
 #include <ctre/Phoenix.h>
+#include <frc/Timer.h>
 using namespace frc;
 
+class Intake{
+    public:
 
+void intake_run();
 
-
-
+    private:
+    Timer time1{};
+    Joystick joy1{1};
+    TalonSRX t1{2};
+    TalonSRX t2{3};
+    Solenoid sol1{PneumaticsModuleType::CTREPCM , 0};
+    Solenoid sol2{PneumaticsModuleType::CTREPCM , 1};
+};
 
 
 #endif
