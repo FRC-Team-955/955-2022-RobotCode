1e8c914373f415213d4b2f6994959a3c10300897
richard4583 <97572875+richard4583@users.noreply.github.com>
2022-01-20 19:03:12 -0800

    Shooter3
    We added two control types, PercentOutput controls and the ability to control the motors independently.

--- src/main/cpp/shooter.cpp
+++ src/main/cpp/shooter.cpp
@@ -2,7 +2,22 @@
 using namespace std; 
 using namespace frc; 
 
-void Shooter::ShootAtVelocity(int V)
-{
-Motor.Set(ControlMode::Velocity, V);
+float Shooter::ShootAtVelocity(int Velocity) {
+    shootertalon_lead.Set(ControlMode::Velocity, Velocity);
+    shootertalon_follow.Set(ControlMode::Follower, 1);
+
+    return shootertalon_lead.GetSelectedSensorVelocity(0);
+}
+
+void Shooter::ShootPercentOutput(int Percent){
+    shootertalon_lead.Set(ControlMode::PercentOutput, Percent);
+}
+
+float Shooter::IndependentControl(int VelocityLead, int VelocityFollow) {
+    shootertalon_lead.Set(ControlMode::Velocity, VelocityLead);
+    shootertalon_follow.Set(ControlMode::Velocity, VelocityFollow);
+
+    
+
+    return shootertalon_lead.GetSelectedSensorVelocity(0);
 }
--- src/main/include/shooter.h
+++ src/main/include/shooter.h
@@ -1,18 +1,23 @@
 #ifndef SHOOTER
 #define SHOOTER
-#include <iostream> 
+#include <iostream>
 #include "frc/Joystick.h"
-#include <ctre/Phoenix.h>
+#include "ctre/Phoenix.h"
 using namespace std;
-using namespace frc; 
+using namespace frc;
 
-class Shooter
-{
-public: 
-void ShootAtVelocity(int V);
+class Shooter {
+    Shooter(){shootertalon_follow.SetInverted(1);}
+    public:
+    float ShootAtVelocity(int Velocity);
+    void ShootPercentOutput(int Percent);
+    float IndependentControl(int VelocityLead, int VelocityFollow);
 
 
-private: 
-TalonSRX Motor {1}; 
+    private:
+    TalonSRX shootertalon_lead {1};
+    TalonSRX shootertalon_follow {2};
+
 };
+
 #endif
