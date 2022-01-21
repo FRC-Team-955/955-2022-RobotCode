49ca52e2b07f45dd5903eedcc439c0e47b9f5658
FarisAri <farisarry@gmail.com>
2022-01-20 19:59:46 -0800

    added more motor hopper percentages

--- src/main/include/hopper.h
+++ src/main/include/hopper.h
@@ -7,13 +7,13 @@ using namespace frc;
 class Hopper{
     public:
         void Run_Hopper_Motor(int hopper_motor_percent1, int hopper_motor_percent2, int hopper_motor_percent3);
-    private:
-        TalonSRX talon_hopper1{MechanismConst::khopper_motor_number};
-        TalonSRX talon_hopper2{MechanismConst::khopper_motor_number};
-        TalonSRX talon_hopper3{MechanismConst::khopper_motor_number};
         int hopper_motor_percent1 = 1;
         int hopper_motor_percent2 = 2;
         int hopper_motor_percent3 = 3;
+    private:
+        TalonSRX talon_hopper1{MechanismConst::khopper_motor_number1};
+        TalonSRX talon_hopper2{MechanismConst::khopper_motor_number2};
+        TalonSRX talon_hopper3{MechanismConst::khopper_motor_number3};
 };
 
 #endif
--- src/main/include/settings.h
+++ src/main/include/settings.h
@@ -20,7 +20,10 @@ constexpr int kright_follow_neo_number = 5;
 } // namespace DriveConst
 
 namespace MechanismConst {
-    constexpr int khopper_motor_number = 1;
+    constexpr int khopper_motor_number1 = 1;
+    constexpr int khopper_motor_number2 = 2;
+    constexpr int khopper_motor_number3 = 3;
+    constexpr int kvelocity_range = 1;
 }
 
 namespace AutoConst {}
