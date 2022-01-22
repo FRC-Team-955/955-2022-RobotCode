c4fbc324ec32e6dbad274f7fc9c34e80730cb42f
FarisAri <farisarry@gmail.com>
2022-01-21 19:25:44 -0800

    save code

---
+++ src/main/cpp/hopper.cpp
@@ -0,0 +1,6 @@
+#include "hopper.h"
+
+void Hopper::Run_Hopper_Motor(int hopper_motor_percent1, int hopper_motor_percent2){
+    talon_hopper1.Set(ControlMode::PercentOutput, hopper_motor_percent1);
+    talon_hopper2.Set(ControlMode::PercentOutput, hopper_motor_percent2);
+}
