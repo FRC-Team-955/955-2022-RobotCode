6f6130ed74dc589ef49dd85cecc8b355e7305ece
richard4583 <97572875+richard4583@users.noreply.github.com>
2022-01-27 19:57:54 -0800

    Alex
    Alex wants to work on it with us

--- src/main/cpp/shooter.cpp
+++ src/main/cpp/shooter.cpp
@@ -1,14 +1,24 @@
 #include "shooter.h"
-using namespace frc; 
+using namespace rev;
 
 float Shooter::ShootAtVelocity(int velocity) {
-    shootertalon_lead.Set(ControlMode::Velocity, velocity);
+    //shooterneo_lead.Set()
+    CANSparkMax::Follow(const shooterneo_lead);
 
     return shootertalon_lead.GetSelectedSensorVelocity(0);
 }
 
 void Shooter::ShootPercentOutput(int percent){
-    shootertalon_lead.Set(ControlMode::PercentOutput, percent);
+    //shootertalon_lead.Set(percent);
 }
+
+void IndependentControl(int leadvelocity, int followvelocity){
+    CANSparkMax::kFollowerDisabled
+    shooterneo_lead.Set
+    shooterneo.follow.Set
+}
+
+
+
 
 
--- src/main/include/shooter.h
+++ src/main/include/shooter.h
@@ -3,17 +3,24 @@
 #include <iostream>
 #include "frc/Joystick.h"
 #include "ctre/Phoenix.h"
+#include "rev/CANSparkMax.h"
 using namespace frc;
 
 class Shooter {
-    Shooter(){}
+    Shooter(){
+        shooterneo_lead.SetSmartCurrentLimit(40); 
+        shooterneo_follow.SetSmartCurrentLimit(40);
+        }
     public:
     float ShootAtVelocity(int velocity);
     void ShootPercentOutput(int percent);
+    void IndependentControl(int leadvelocity, int followvelocity);
 
 
     private:
-    TalonSRX shootertalon_lead {1};
+    CANSparkMax shooterneo_lead {DriveConst::kright_lead_neo_number, CANSparkMax::MotorType::kBrushless};
+    CANSparkMax shooterneo_follow {DriveConst::kright_lead_neo_number, CANSparkMax::MotorType::kBrushless};
+
 
 };
 
