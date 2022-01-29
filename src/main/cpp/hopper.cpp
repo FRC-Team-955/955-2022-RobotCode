6ec48903fb7a70e8f40c4c3ff1ccdb6b4d889f36
FarisAri <farisarry@gmail.com>
2022-01-28 18:25:24 -0800

    added min() in the motor percent and max

--- src/main/cpp/hopper.cpp
+++ src/main/cpp/hopper.cpp
@@ -1,6 +1,6 @@
 #include "hopper.h"
 
 void Hopper::Run_Hopper_Motor(int hopper_motor_percent_top, int hopper_motor_percent_bottom){
-    talon_hopper_top.Set(ControlMode::PercentOutput, hopper_motor_percent_top * max);
-    talon_hopper_bottom.Set(ControlMode::PercentOutput, hopper_motor_percent_bottom * max);
+    talon_hopper_top.Set(ControlMode::PercentOutput, std::min(hopper_motor_percent_top, max));
+    talon_hopper_bottom.Set(ControlMode::PercentOutput, std::min(hopper_motor_percent_bottom, max));
 }
--- src/main/include/hopper.h
+++ src/main/include/hopper.h
@@ -2,6 +2,7 @@
 #define HOPPER
 #include <ctre/Phoenix.h>
 #include "settings.h"
+#include <algorithm>
 using namespace frc;
 
 class Hopper{
@@ -12,7 +13,7 @@ class Hopper{
             talon_hopper_top.TalonSRX::EnableCurrentLimit(true);
             talon_hopper_bottom.TalonSRX::EnableCurrentLimit(true);
         }
-        int max = -1;
+        int max = 1;
         void Run_Hopper_Motor(int hopper_motor_percent_top, int hopper_motor_percent_bottom);
     private:
         TalonSRX talon_hopper_top{MechanismConst::khopper_motor_number1};
