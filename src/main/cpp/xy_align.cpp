f9a09c31e10ff1bdfad675255bd7ff35b6597444
LukeWilliams <47074748+LukeWilliams271@users.noreply.github.com>
2022-01-28 18:21:07 -0800

    Save

--- src/main/cpp/xy_align.cpp
+++ src/main/cpp/xy_align.cpp
@@ -35,6 +35,24 @@ void XYalign::test()
     float distance_adjust = KpDistance * distance_error; //Set the variable in shooter code
     steering_adjust = Kp*heading_error;
 
+    //experimental shooting code, uses the matrix to solve for the needed parabola to hit the shot
+    float rim_point_x = 2;
+    float rim_point_x = 9;
+    float shooter_position_y = 2;
+    float distance;
+
+    double matrix[2][2];
+
+    matrix[0][0] = rim_point_x * rim_point_x;
+    matrix[1][0] = rim_point_x;
+    matrix[0][1] = distance * distance;
+    matrix[1][1] = distance;
+
+    float parabola_a = 
+
+
+
+
     drivebase->DriveTank(steering_adjust, -steering_adjust);
   }
 }
