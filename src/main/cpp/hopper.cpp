1afc0f4c4635de8021d7dcee150bcd58a419991e
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-01-30 12:09:44 -0800

    naming/formatting

--- src/main/cpp/hopper.cpp
+++ src/main/cpp/hopper.cpp
@@ -1,6 +1,9 @@
 #include "hopper.h"
 
-void Hopper::Run_Hopper_Motor(int hopper_motor_percent_top, int hopper_motor_percent_bottom){
-    talon_hopper_top.Set(ControlMode::PercentOutput, std::min(hopper_motor_percent_top, max));
-    talon_hopper_bottom.Set(ControlMode::PercentOutput, std::min(hopper_motor_percent_bottom, max));
+void Hopper::RunHopperMotor(int hopper_motor_percent_top,
+                            int hopper_motor_percent_bottom) {
+  talon_hopper_top.Set(ControlMode::PercentOutput,
+                       std::min(hopper_motor_percent_top, max));
+  talon_hopper_bottom.Set(ControlMode::PercentOutput,
+                          std::min(hopper_motor_percent_bottom, max));
 }
--- src/main/include/hopper.h
+++ src/main/include/hopper.h
@@ -1,22 +1,25 @@
 #ifndef HOPPER
 #define HOPPER
 #include "settings.h"
-#include <ctre/Phoenix.h>
 #include <algorithm>
+#include <ctre/Phoenix.h>
 
-class Hopper{
-    public:
-        Hopper(){
-            talon_hopper_top.TalonSRX::ConfigPeakCurrentLimit(40);
-            talon_hopper_bottom.TalonSRX::ConfigPeakCurrentLimit(40);
-            talon_hopper_top.TalonSRX::EnableCurrentLimit(true);
-            talon_hopper_bottom.TalonSRX::EnableCurrentLimit(true);
-        }
-        int max = 1;
-        void Run_Hopper_Motor(int hopper_motor_percent_top, int hopper_motor_percent_bottom);
-    private:
-        TalonSRX talon_hopper_top{MechanismConst::khopper_motor_top_port};
-        TalonSRX talon_hopper_bottom{MechanismConst::khopper_motor_bottom_port};
+
+class Hopper {
+public:
+  Hopper() {
+    talon_hopper_top.TalonSRX::ConfigPeakCurrentLimit(40);
+    talon_hopper_bottom.TalonSRX::ConfigPeakCurrentLimit(40);
+    talon_hopper_top.TalonSRX::EnableCurrentLimit(true);
+    talon_hopper_bottom.TalonSRX::EnableCurrentLimit(true);
+  }
+  int max = 1;
+  void RunHopperMotor(int hopper_motor_percent_top,
+                      int hopper_motor_percent_bottom);
+
+private:
+  TalonSRX talon_hopper_top{MechanismConst::khopper_motor_top_port};
+  TalonSRX talon_hopper_bottom{MechanismConst::khopper_motor_bottom_port};
 };
 
 #endif
