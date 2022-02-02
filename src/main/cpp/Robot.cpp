914b348ec4aca5b9a5474c08f80778d3c6a4297d
jjxfan <46433103+jjxfan@users.noreply.github.com>
2022-02-01 20:38:54 -0800

    Save

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,7 +11,9 @@
 
 #include "colorsensor.h"
 #include "drivebase.h"
-// #include "feedbacksystem.h"
+
+#include <frc/Timer.h>
+#include "feedbacksystem.h"
 
 using namespace frc;
 
@@ -19,17 +21,20 @@ Joystick *joystick;
 
 DriveBase *drivebase;
 ColorSensor *color_sensor;
+frc::Timer* m_timer;
+
 frc::SendableChooser<std::string> m_positionChooser;
 
 void Robot::RobotInit() {
+  m_timer = new frc::Timer();
   drivebase = new DriveBase();
   color_sensor = new ColorSensor();
   frc::CameraServer::GetInstance()->StartAutomaticCapture();
-
   frc::ShuffleboardTab& tabpre = Shuffleboard::GetTab("Pre");
-  frc::ShuffleboardTab& tabauto = Shuffleboard::GetTab("Auto");
-  frc::ShuffleboardTab& tabtelop = Shuffleboard::GetTab("Telop");
   frc::ShuffleboardTab& tableend = Shuffleboard::GetTab("End Game");
+  frc::ShuffleboardTab& tabtelop = Shuffleboard::GetTab("Telop");
+  frc::ShuffleboardTab& tabauto = Shuffleboard::GetTab("Auto");
+
 
   m_positionChooser.AddOption("Left","Left");
   m_positionChooser.AddOption("Right","Right");
@@ -42,15 +47,24 @@ void Robot::RobotInit() {
 void Robot::RobotPeriodic() {}
 void Robot::AutonomousInit() {
   Shuffleboard::SelectTab("Auto");
-  // m_autoposition  = Shuffleboard::GetTab("Pre").Add
 }
 void Robot::AutonomousPeriodic() {}
 void Robot::TeleopInit() {
+
   Shuffleboard::SelectTab("Telop");
+  m_timer->Start();
 
 }
-void Robot::TeleopPeriodic() { 
+void Robot::TeleopPeriodic() {
+
   drivebase->Drive();
+
+  //Change to if (m_timer->GetMatchTime() > 10_s){ for the final version
+  if (m_timer->Get() > 10_s){
+    Shuffleboard::SelectTab("End Game");
+  }
+
+  // ShuffleBoard Stuff
   drivebase->DisplayDriveInfo();
 
  }
