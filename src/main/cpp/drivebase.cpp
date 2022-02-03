2475de5af297290004b1230532ba46cf6d54c25e
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2022-02-03 15:19:16 -0800

    #57 added debounce
    hope this works cause if it doesn't I messed up beta

--- src/main/cpp/drivebase.cpp
+++ src/main/cpp/drivebase.cpp
@@ -1,25 +1,17 @@
 #include "drivebase.h"
-#include <frc/smartdashboard/SmartDashboard.h>
 
 void DriveBase::Drive() {
 
-  bool isQuickTurn = joystick.GetRawButton(Joy0Const::kquick_turn_button);
+  isQuickTurn = joystick.GetRawButton(Joy0Const::kquick_turn_button);
 
-  if (joystick.GetRawButtonPressed(Joy0Const::kreverse_drive)) {
-    ReverseDrive = !ReverseDrive;
-  }
-  if (ReverseDrive == false) {
-    m_robotDrive.CurvatureDrive(
-        joystick.GetRawAxis(Joy0Const::kdrive_speed_axis),
-        joystick.GetRawAxis(Joy0Const::kdrive_curvature_axis), isQuickTurn);
-    frc::SmartDashboard::PutBoolean("Reverse Drive", ReverseDrive);
-
-  } else {
+  if (buttontoggle.GetToggle(joystick.GetRawButton(Joy0Const::kreverse_drive))) {
     m_robotDrive.CurvatureDrive(
         -joystick.GetRawAxis(Joy0Const::kdrive_speed_axis),
         -joystick.GetRawAxis(Joy0Const::kdrive_curvature_axis), isQuickTurn);
-    frc::SmartDashboard::PutBoolean("Reverse Drive", ReverseDrive);
-  }
 
-  // m_pidController.SetReference(rotations, rev::ControlType::kPosition);
+  } else {
+    m_robotDrive.CurvatureDrive(
+        joystick.GetRawAxis(Joy0Const::kdrive_speed_axis),
+        joystick.GetRawAxis(Joy0Const::kdrive_curvature_axis), isQuickTurn);
+  }
 }
