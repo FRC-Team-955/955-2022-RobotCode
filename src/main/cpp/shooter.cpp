148b3ab18be1f90044df2d4d1d63aed473a2e76e
jjxfan <46433103+jjxfan@users.noreply.github.com>
2022-02-03 20:07:37 -0800

    pushing shooter

--- src/main/cpp/shooter.cpp
+++ src/main/cpp/shooter.cpp
@@ -1,11 +1,23 @@
 #include "shooter.h"
 using namespace frc;
+using namespace rev;
 
 float Shooter::ShootAtVelocity(int velocity) {
   shooter_talon.Set(ControlMode::Velocity, velocity);
   return shooter_talon.GetSelectedSensorVelocity(0);
+void Shooter::ShootPercentOutput(int percent){
+    shooterneo_lead.Set(percent);
 }
 
 void Shooter::ShootPercentOutput(int percent) {
   shooter_talon.Set(ControlMode::PercentOutput, percent);
+} 
+float IndependentControl(int leadvelocity, int followvelocity){
+    double SetPoint = 0.0;
+    m_pidController.SetReference(SetPoint, rev::ControlType::kVelocity);
+    m_pidController2.SetReference(SetPoint, rev::ControlType::kVelocity);
+
+    return shooterneo_lead.GetEncoder().GetVelocity();
+    return shooterneo_follow.GetEncoder().GetVelocity();
+
 }
--- src/main/include/settings.h
+++ src/main/include/settings.h
@@ -28,7 +28,9 @@ constexpr bool kleft_lead_is_inverted = false;
 constexpr bool kright_lead_is_inverted = true;
 } // namespace DriveConst
 
-namespace MechanismConst {
+namespace MechanismConst {double kP = 1, kI = 1, kD = 1, kIz = 1, kFF = 1, kMaxOutput = 1, kMinOutput = 1;
+constexpr int shootertalon_follow = 1; 
+constexpr int shooter_talon = 8;
 constexpr int khopper_motor_top_port = 1;
 constexpr int khopper_motor_bottom_port = 2;
 constexpr int kshooter_talon_port = 8;
@@ -38,6 +40,6 @@ constexpr int kelevator_motor_port = 6;
 constexpr int kelevator_pnumatic_port = 0;
 constexpr int climb_amperage = 10;
 constexpr int elevator_slow_multiplier = 0.5;
-}
+};
 
 namespace AutoConst {}
