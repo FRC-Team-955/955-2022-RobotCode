217684a0df0e54ee9dfc9bd19aa50a8475c77cee
jjxfan <46433103+jjxfan@users.noreply.github.com>
2022-02-04 14:20:39 -0800

    update elevator and remove breakbeam

--- src/main/cpp/elevator.cpp
+++ src/main/cpp/elevator.cpp
@@ -6,23 +6,24 @@ void Elevator::ElevatorMove(int joystick_position) {
   if (solenoid0.Get() == 1) {
     elevator_motor.SetNeutralMode(NeutralMode::Brake);
     elevator_motor.Set(ControlMode::PercentOutput, 0);
-  }
-  if (limit_switch_top.Get() == 1 && joystick_position > 0) {
-    elevator_motor.SetNeutralMode(NeutralMode::Brake);
-    elevator_motor.Set(ControlMode::PercentOutput, 0);
-  } else if (limit_switch_top.Get() == 1 && joystick_position < 0) {
-    elevator_motor.Set(ControlMode::PercentOutput, joystick_position);
-  }
-  if (limit_switch_bottom.Get() == 1 && joystick_position < 0) {
-    elevator_motor.SetNeutralMode(NeutralMode::Brake);
-    elevator_motor.Set(ControlMode::PercentOutput, 0);
-  } else if (limit_switch_bottom.Get() == 1 && joystick_position > 0) {
-    elevator_motor.Set(ControlMode::PercentOutput, joystick_position);
-  }
-  if (elevator_motor.GetSelectedSensorPosition() > 4000) {
-    elevator_motor.Set(ControlMode::PercentOutput, joystick_position * 0.5);
-  } else if (elevator_motor.GetSelectedSensorPosition() < 1000) {
-    elevator_motor.Set(ControlMode::PercentOutput, joystick_position * 0.5);
+  } else {
+    if (limit_switch_top.Get() == 1 && joystick_position > 0) {
+      elevator_motor.SetNeutralMode(NeutralMode::Brake);
+      elevator_motor.Set(ControlMode::PercentOutput, 0);
+    } else if (limit_switch_top.Get() == 1 && joystick_position < 0) {
+      elevator_motor.Set(ControlMode::PercentOutput, joystick_position);
+    }
+    if (limit_switch_bottom.Get() == 1 && joystick_position < 0) {
+      elevator_motor.SetNeutralMode(NeutralMode::Brake);
+      elevator_motor.Set(ControlMode::PercentOutput, 0);
+    } else if (limit_switch_bottom.Get() == 1 && joystick_position > 0) {
+      elevator_motor.Set(ControlMode::PercentOutput, joystick_position);
+    }
+    if (elevator_motor.GetSelectedSensorPosition() > 4000) {
+      elevator_motor.Set(ControlMode::PercentOutput, joystick_position * 0.5);
+    } else if (elevator_motor.GetSelectedSensorPosition() < 1000) {
+      elevator_motor.Set(ControlMode::PercentOutput, joystick_position * 0.5);
+    }
   }
 }
 
